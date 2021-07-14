30078df2650d1367e6d88319336f9604ae495a72
Ken V <ken.valverde@yahoo.com>
2021-07-13 19:16:54 -0500

    Extract and read the high temperatures

--- sitka_highs.py
+++ sitka_highs.py
@@ -3,9 +3,17 @@ import csv
 filename = "data/sitka_weather_07-2018_simple.csv"
 with open(filename) as f:
     reader = csv.reader(f)
-    header_row = next(reader)
+    header_row = next(reader)  # call next() once to get the first line of the file
 
     for index, column_header in enumerate(header_row):
         print(index, column_header)
+
+    # Get the high temps
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+
+    print(highs)
 
 
